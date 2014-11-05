3125fa966c4f74566e7b91476c9af09af24a2624
gannimet <r.wotzlaw@gmail.com>
2014-11-05 14:13:09 +0100

    promise revert because data wouldn't load otherwise

--- src/client/js/controllers.js
+++ src/client/js/controllers.js
@@ -3,10 +3,9 @@
 	var blogControllers = angular.module('blogControllers', []);
 
 	blogControllers.controller('BlogCtrl', ['$scope', 'entryService', function($scope, entryService) {
-		entryService.allEntries().then(function(data, status) {
+		entryService.allEntries().success(function(data, status) {
 			$scope.entries = data.entries;
 		});
-		$scope.hello = 'world';
 	}]);
 
 	blogControllers.controller('EntryCtrl', ['$scope', 'entryService', '$routeParams',
