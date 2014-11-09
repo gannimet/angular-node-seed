00c092ff87f279da5936942996d3ce07dec8da82
gannimet <r.wotzlaw@gmail.com>
2014-11-09 13:42:58 +0100

    Show number of comments via angular filter

--- src/client/js/filters.js
+++ src/client/js/filters.js
@@ -2,4 +2,18 @@
 
 	var blogFilters = angular.module('blogFilters', []);
 
+	blogFilters.filter('numberOfComments', function() {
+		return function(commentsCount) {
+			if (commentsCount < 1) {
+				return 'No comments yet';
+			}
+
+			if (commentsCount === 1) {
+				return '1 comment:';
+			}
+
+			return commentsCount + ' comments:';
+		};
+	});
+
 })(window);
